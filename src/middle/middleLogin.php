8471afaa2fdf62624e7c7a7b5b80e9ef3f15c9e8
Kendy Colon <kcolon018@gmail.com>
2018-01-29 13:18:26 -0500

    middle folder designated for all the files for the middle by Kendy(Middle)

---
+++ src/middle/middleLogin.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by KendyColon.
+ * User: kendycolon
+ * Date: 1/26/18
+ * Time: 1:40 AM
+ */
+
+
+
+$capture = file_get_contents("php://input");
+$connection = curl_init();
+
+curl_setopt($connection, CURLOPT_URL, "http://afsaccess1.njit.edu/~USERNAME/DIRECTORY/middleLogin.php");
+curl_setopt($connection, CURLOPT_POST, 1);
+curl_setopt($connection, CURLOPT_POSTFIELDS, $capture);
+curl_setopt($connection, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($connection, CURLOPT_RETURNTRANSFER, 1);
+
+//SEND REQUEST TO THE FILE IN THE NJIT ACCOUNT
+$designation = curl_exec($connection);
+//FINISH CONNECTION
+curl_close($connection);
+
+?>
+
+?>
