69a7fec90c0a45f7ba0c09fb6a1c20ae79e615c5
Anderson Reyes <reyesanderson428@gmail.com>
2018-01-31 01:48:59 -0500

    .

--- src/middle/middleLogin.php
+++ src/middle/middleLogin.php
@@ -8,20 +8,18 @@
 
 
 //CAPTURE THE DATA SENT FROM THE FRONT-END
-// $capture = file_get_contents("php://input");
-// $connection = curl_init();
+$capture = file_get_contents("php://input");
+$connection = curl_init();
 
-// curl_setopt($connection, CURLOPT_URL, "http://afsaccess1.njit.edu/~USERNAME/DIRECTORY/middleLogin.php");
-// curl_setopt($connection, CURLOPT_POST, 1);
-// curl_setopt($connection, CURLOPT_POSTFIELDS, $capture);
-// curl_setopt($connection, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-// curl_setopt($connection, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($connection, CURLOPT_URL, "http://afsaccess1.njit.edu/~USERNAME/DIRECTORY/middleLogin.php");
+curl_setopt($connection, CURLOPT_POST, 1);
+curl_setopt($connection, CURLOPT_POSTFIELDS, $capture);
+curl_setopt($connection, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($connection, CURLOPT_RETURNTRANSFER, 1);
 
-// //SEND REQUEST TO THE FILE IN THE NJIT ACCOUNT
-// $designation = curl_exec($connection);
-// //FINISH CONNECTION
-// curl_close($connection);
-
-print_r($_POST);
+//SEND REQUEST TO THE FILE IN THE NJIT ACCOUNT
+$designation = curl_exec($connection);
+//FINISH CONNECTION
+curl_close($connection);
 
 ?>
