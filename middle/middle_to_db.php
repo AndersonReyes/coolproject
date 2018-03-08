a43ae98ab551c38d6691574457a45a0d681df464
Anderson Reyes <reyesanderson428@gmail.com>
2018-03-07 19:12:10 -0500

    uploaded a file that will handle all curls from front to databasee to make it easy and stay with prof architecture

---
+++ middle/middle_to_db.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once "./../../utils/php_utils.php";
+
+
+// This will simply pass the post to the database and return the results
+$result = post_curl($_POST, "https://web.njit.edu/~ssc3/coolproject/beta/database.php");
+ echo json_encode($result);
