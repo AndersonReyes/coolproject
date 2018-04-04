31f664ab7e8205063e2e8696144765b611afb0b1
Anderson Reyes <reyesanderson428@gmail.com>
2018-04-03 22:09:59 -0400

    take quiz student got updated to new database get quiz view

--- frontend/views/homepage_student.php
+++ frontend/views/homepage_student.php
@@ -37,7 +37,7 @@ $quizzes = post_curl($arr, "https://web.njit.edu/~krc9/coolproject/middle/middle
                     foreach ($quizzes as $quiz_name => $quiz_info) {
                         $published = $quiz_info[0]["publish"];
                         $disable = "";
-
+			
                         if ($published === "TRUE") {
                             echo "<form method='POST' action='view_quiz_student.php'>
                             <input type='hidden' name='graded' value='FALSE'>
@@ -49,7 +49,9 @@ $quizzes = post_curl($arr, "https://web.njit.edu/~krc9/coolproject/middle/middle
                             <td><input type='submit' name='view_graded' value='view' {$disable}></td>
                             </tr>
                             </form>";
-                        } else if (($published === "FALSE" || $published === "NULL" || published === null) && $quiz["a1"] === "") {
+                        } else if (($published === "FALSE" || $published ===
+			"NULL" || $published === null) &&
+			$quiz_info[0]["answer"] === null) {
 
                                 echo "<form method='POST' action='view_quiz_student.php'>
                                 <input type='hidden' name='graded' value='FALSE'>
