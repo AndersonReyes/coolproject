fc7dbfffae7b6f9fb1a331cb82855b1c55bcfe3c
Anderson Reyes <reyesanderson428@gmail.com>
2018-04-06 16:49:34 -0400

    Updated the student header to display 'Student' on the left instead of 'Username'

--- frontend/views/header_student.php
+++ frontend/views/header_student.php
@@ -1,7 +1,7 @@
 <div class="header">
 
-    <div class="navbar">          
-        <a class="nav-item" id="username">Username</a>
+    <div class="navbar">
+        <a class="nav-item" id="username">Student</a>
         <a href="../../index.html" class="nav-item" id="logout">Logout</a>
     </div>
 </div>
